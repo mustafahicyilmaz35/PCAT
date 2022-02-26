d0dc9c307dab6d13064b7e5ff963af485acb95fc
mustafahicyilmaz35 <mustafahicyilmaz35@gmail.com>
2022-02-26 22:02:48 +0300

    static file uploaded

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
-const fn = function () {
-  console.log('Function Working');
-};
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+//MIDDLEWARES
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve(__dirname, 'temp/index.html'));
+});
+
+const port = 3000;
+
+app.listen(port, () => {
+  console.log(`sunucu ${port} portunda başlatıldı`);
+});
